65f8dacb3afb33010f6b8b4365f1788f207faa1a
sagdish <forsi1@yahoo.com>
2020-10-01 17:21:31 -0700

    Day 1 complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+    
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +70,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.load()
+        running = True
+
+        while running:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == 0b10000010:
+                self.reg[operand_a] = operand_b
+                self.pc += 2
+            elif IR == 0b01000111:
+                print(self.reg[operand_a])
+                self.pc += 1
+            elif IR == 0b00000001:
+                running = False
+            self.pc +=1
+
+
+            
+
+
+
+        
