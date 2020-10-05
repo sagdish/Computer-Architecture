7968df25bd1a8612a3ad2525c2be928c13594d87
sagdish <forsi1@yahoo.com>
2020-10-04 22:55:20 -0700

    day 2 mult operation support and helper function to identify operation

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
@@ -10,41 +11,79 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
-    
+
     def ram_read(self, MAR):
         return self.ram[MAR]
-    
+
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
 
+    def alu_identifier(self, num):
+        if num == 0b0000:
+            return "ADD"
+        elif num == 0b1000:
+            return "AND"
+        elif num == 0b0111:
+            return "CMP"
+        elif num == 0b0110:
+            return "DEC"
+        elif num == 0b0011:
+            return "DIV"
+        elif num == 0b0101:
+            return "INC"
+        elif num == 0b0100:
+            return "MOD"
+        elif num == 0b0010:
+            return "MUL"
+        elif num == 0b1001:
+            return "NOT"
+        elif num == 0b1010:
+            return "OR"
+        elif num == 0b1100:
+            return "SHL"
+        elif num == 0b1101:
+            return "SHR"
+        elif num == 0b0001:
+            return "SUB"
+        elif num == 0b1011:
+            return "XOR"
+        else:
+            return 'error'
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] /= self.reg[reg_b]
+        elif op == "INC":
+            self.reg[reg_a] += 1
+        elif op == "DEC":
+            self.reg[reg_a] -= 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -56,8 +95,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -70,27 +109,31 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        self.load()
         running = True
 
         while running:
             IR = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
+            num_of_operands = IR >> 6
+            alu_oper = True if IR & 0b00100000 == 32 else False
+            # print('alu?', alu_oper)
 
-            if IR == 0b10000010:
+            # self.trace()
+            if IR == 0b10000010:  # LDI
                 self.reg[operand_a] = operand_b
-                self.pc += 2
-            elif IR == 0b01000111:
+                self.pc += num_of_operands
+            elif alu_oper:
+                num = IR & 0b00001111
+                # print('mul is 2', num)
+                operation = self.alu_identifier(num)
+                # print('operation', operation)
+                self.alu(operation, operand_a, operand_b)
+                self.pc += num_of_operands
+            elif IR == 0b01000111:  # PRN
                 print(self.reg[operand_a])
-                self.pc += 1
-            elif IR == 0b00000001:
+                self.pc += num_of_operands
+            elif IR == 0b00000001:  # HLT
                 running = False
-            self.pc +=1
 
-
-            
-
-
-
-        
+            self.pc += 1
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,28 @@
 import sys
 from cpu import *
 
+program = []
+
+if (len(sys.argv)) != 2:
+    print("Please pass the second file name")
+    print("usage: python3 fileio.py <second_file_name.py>")
+    sys.exit()
+
+try:
+    with open(sys.argv[1]) as f:
+        for line in f:
+            possible_num = line[:line.find('#')]
+            if possible_num == '':
+                continue
+            integer = int(possible_num, 2)
+            program.append(integer)
+except FileNotFoundError:
+    print(f'Error from {sys.argv[0]}: {sys.argv[1]} not found')
+    sys.exit()
+
+# print('program', program)
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(program)
 cpu.run()
